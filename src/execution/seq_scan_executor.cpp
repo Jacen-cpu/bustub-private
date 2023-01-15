093cf78de5d6868832be9b368baeb6374067ef72
G1g4lo <3163083993@qq.com>
2023-01-15 17:43:43 +0800

    seq

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,29 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "common/config.h"
+#include "common/rid.h"
+#include "storage/page/header_page.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) 
+    : AbstractExecutor(exec_ctx), plan_(plan), table_iter_(nullptr, RID(INVALID_PAGE_ID, 0), nullptr) { }
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() { 
+    table_info_ = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+    table_iter_ = table_info_->table_->Begin(exec_ctx_->GetTransaction()); 
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
-
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+    if (table_iter_ == table_info_->table_->End()) {
+        return false;
+    } 
+    *tuple = *table_iter_;
+    *rid = tuple->GetRid();
+    table_iter_++;
+    return true;
+}
 }  // namespace bustub
